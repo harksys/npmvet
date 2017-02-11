329f1c104ada6658368b2285f3291e7525efcb25
Tim Jacobi <dev@tim-jacobi.com>
2017-02-11 13:11:31 +0000

    implement json renderer

---
+++ src/renderers/json.ts
@@ -0,0 +1,25 @@
+export const render: IRenderer = (depMap) =>
+{
+  /*
+   * Merge dependencies and dev dependencies
+   */
+  const allPackages = depMap.deps.concat(depMap.devDeps);
+
+  /*
+   * Print the JSON
+   */
+  console.log(JSON.stringify(createMatchInfos(allPackages), undefined, 2));
+};
+
+/**
+ * @param  {IPackageDescriptor[]} pkgDescriptor
+ * @returns {IPackageMatchInfo[]}
+ */
+let createMatchInfos = (pkgDescriptor: IPackageDescriptor[]): IPackageMatchInfo[] =>
+  pkgDescriptor.map(pkg => ({    
+    name: pkg.name,
+    packageVersion: pkg.parsedDefinedVersion,
+    installedVersion: pkg.installedVersion,
+    matches: pkg.parsedDefinedVersion !== pkg.installedVersion,
+    locked: pkg.locked
+  }));
