551f21e389918a18c2820036b068c3f6bf5d8aaa
Andrew Hathaway <andrew@andrewhathaway.net>
2017-04-04 20:35:10 +0100

    Alter CI rendered to use the new "strict" option to check if unlocked packages exist, failing if so!
    Also fixes a bug where the next line would be covered in the colour as well as the text.

--- src/renderers/ci.ts
+++ src/renderers/ci.ts
@@ -4,46 +4,98 @@ import { render as inlineTableRenderer } from './inlinetable';
 
 import { isMatchingVersion } from '../deps';
 
-export const render: IRenderer = (depMap) =>
+export const render: IRenderer = (depMap, cliOpts) =>
 {
   /*
    * Filter packages to only the ones where versions mismatch
    */
-  const filtered: IPackageDescriptorMap = {
+  const filteredMatching: IPackageDescriptorMap = {
     deps    : filterMatchingDependencies(depMap.deps),
     devDeps : filterMatchingDependencies(depMap.devDeps)
   };
 
+  const filteredUnlocked: IPackageDescriptorMap = {
+    deps : filterLockedDependencies(depMap.deps),
+    devDeps : filterLockedDependencies(depMap.devDeps)
+  };
+
+
   /*
-   * Do we have any mismatches?
+   * Do we have any mismatches or unlocked?
    */
-  const hasMismatches = filtered.deps.length > 0
-                          || filtered.devDeps.length > 0;
+  const hasMismatches = filteredMatching.deps.length > 0
+                          || filteredMatching.devDeps.length > 0;
+
+  const hasUnlocked = filteredUnlocked.deps.length > 0
+                        || filteredUnlocked.devDeps.length > 0;
 
   /*
    * We have no mismatching packages, so lets
    * render the table and a nice message
    */
-  if (!hasMismatches) {
-    console.log(chalk.bold.bgGreen(' NPM Vet: No mismatched package versions \n'));
-
-    inlineTableRenderer(depMap);
-    process.exit(0);
+  if (!cliOpts.strict && !hasMismatches) {
+    sendSuccessResponse(depMap, ' NPM Vet: No mismatched package versions ');
   }
 
   /*
-   * Render an error message and a table with only
-   *  the mismatching verisons shown
+   * If we're in strict mode, and nothing is unlocked,
+   * then render the table and a nice message.
    */
-  const mismatchingLength = filtered.deps.length + filtered.devDeps.length;
-  const phrase            = mismatchingLength > 1
-                              ? pluralize('package')
-                              : 'package';
+  if (cliOpts.strict && !hasUnlocked) {
+    sendSuccessResponse(depMap, ' NPM Vet: No unlocked packages or mismatched package versions ');
+  }
 
-  console.log(chalk.bold.bgRed(` NPM Vet: You have ${mismatchingLength} ${phrase} `
-                               + `with mismatched versions \n`));
+  /*
+   * Whenever we get here, we either have no
+   * some mismatches or some unlocked packages
+   * in strict mode.
+   */
+  const mismatchingLength = filteredMatching.deps.length + filteredMatching.devDeps.length;
+  const unlockedLength    = filteredUnlocked.deps.length + filteredUnlocked.devDeps.length;
 
-  inlineTableRenderer(filtered);
+  const phrase = mismatchingLength > 1
+                  ? pluralize('package')
+                  : 'package';
+
+  /*
+   * Render an error message and a table with only
+   * the mismatching verisons shown if any mismatches
+   * are there.
+   */
+  if (mismatchingLength > 0) {
+    sendErrorResponse(filteredMatching, ` NPM Vet: You have ${mismatchingLength} ${phrase} `
+                                        + `with mismatched versions `);
+  }
+
+  /*
+   * We must be in strict mode with some unlocked
+   * packages, lets fail.
+   */
+  sendErrorResponse(filteredUnlocked, ` NPM Vet: You have ${unlockedLength} ${phrase} `
+                                        + `that are currently unlocked versions `);
+};
+
+/**
+ * @param  {IPackageDescriptorMap} depMap
+ * @param  {string} message
+ */
+let sendSuccessResponse = (depMap: IPackageDescriptorMap, message: string) =>
+{
+  console.log(chalk.bold.bgGreen(message));
+
+  inlineTableRenderer(depMap);
+  process.exit(0);
+};
+
+/**
+ * @param  {IPackageDescriptorMap} filteredMatching
+ * @param  {string} message
+ */
+let sendErrorResponse = (filteredMatching: IPackageDescriptorMap, message: string) =>
+{
+  console.log(chalk.bold.bgRed(message));
+
+  inlineTableRenderer(filteredMatching);
   process.exit(1);
 };
 
@@ -53,3 +105,10 @@ export const render: IRenderer = (depMap) =>
  */
 let filterMatchingDependencies = (deps: IPackageDescriptor[]): IPackageDescriptor[] =>
   deps.filter(d => !isMatchingVersion(d.parsedDefinedVersion, d.installedVersion));
+
+/**
+ * @param  {IPackageDescriptor[]} deps
+ * @returns IPackageDescriptor
+ */
+let filterLockedDependencies = (deps: IPackageDescriptor[]): IPackageDescriptor[] =>
+  deps.filter(d => !d.locked);
