7de48f4fbf0c81194a2fda4d8649a7eec1728c8d
Amir Tahvildaran <amirdt22@gmail.com>
2019-11-03 14:27:51 -0500

    Add failing test for versions not-starting with 'v'

--- test/tests/deps/extractVersionFromUrlTest.js
+++ test/tests/deps/extractVersionFromUrlTest.js
@@ -7,6 +7,7 @@ describe('deps.extractVersionFromUrlTest', () =>
   {
     var deps = require(SRC_DIRECTORY + '/deps');
 
+    expect(deps.extractVersionFromUrl('git+ssh://git@git.testing.abc:test-group/testmodule.git#1.3.2')).toBe('1.3.2');
     expect(deps.extractVersionFromUrl('git+ssh://git@git.testing.abc:test-group/testmodule.git#v1.3.2')).toBe('1.3.2');
     expect(deps.extractVersionFromUrl('git+ssh://git@git.testing.abc:test-group/testmodule.git')).toBe('Unknown');
   });
